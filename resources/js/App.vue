b13e0dcc5621d2e2b896f43fd22ec95cfa57d623
ChanHomseebounheuang <chanhom885@gmail.com>
2023-10-05 11:24:54 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+.<template>
+    <div>
+        <h1>hello vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyChanhomeApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
